97bce37f83194efd5022ea0d55d163db5e76de65
Olavo Shibata <olavoshibata@gmail.com>
2016-06-06 22:00:48 -0300

    update temporary Place

--- controllers/temporaryPlace.js
+++ controllers/temporaryPlace.js
@@ -1,63 +1,9 @@
-var User = require('./../models/user');
-var TemporaryPlace = require('./../models/temporaryPlace');
+var User = require('./../models/user')
+var TemporaryPlace = require('./../models/temporaryPlace')
 
 var TemporaryPlaceController = {
   index: (req, res) => {
     TemporaryPlace.find({organizationId: req.params.id}).execAsync()
-<<<<<<< Updated upstream
-    .then((tempPlaces) => {
-      res.render('temporaryPlace/index', { userActive: req.user, tempPlaces: tempPlaces });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.render('error', { error: err });
-    })
-  }
-  // ,
-  // show: function(req, res) {
-  //   User.findById(req.params.id).execAsync()
-  //   .then(function(user) {
-  //     res.render('profile/show', { user: user });
-  //   })
-  //   .catch(function(err){
-  //     console.log('error:', err);
-  //     return err;
-  //   });
-  // },
-  // create: function(req, res) {
-  //   User.findById(req.user._id).execAsync()
-  //   .then(function(user) {
-  //     for (var key in req.body.user) {
-  //       user[key] = req.body.user[key];
-  //     }
-  //     user['stage'] = 1;
-  //     return user.saveAsync();
-  //   })
-  //   .then(function(user) {
-  //     var org = new Organization();
-  //     for (var key in req.body.organization) {
-  //       org[key] = req.body.organization[key];
-  //     }
-  //     return org.saveAsync();
-  //   })
-  //   .then(function(org){
-  //     req.body.shelter.forEach(function(shelter, index){
-  //       var newShelter = new Shelter();
-  //       newShelter.organizationId = org._id;
-  //       for (var key in shelter) {
-  //         newShelter[key] = shelter[key];
-  //       }
-  //       newShelter.saveAsync();
-  //     });
-  //     return res.redirect('/dashboard'); 
-  //   })
-  //   .catch(function(err){
-  //     console.log('error:', err);
-  //     return err;
-  //   });
-  // }
-};
-=======
       .then((tempPlaces) => {
         res.render('temporaryPlace/index', { userActive: req.user, tempPlaces: tempPlaces })
       })
@@ -85,7 +31,7 @@ var TemporaryPlaceController = {
 
     temporaryPlace.save((err, temporaryPlace) => {
       if (err)
-        res.send(err)
+        res.render('error', { error: err })
 
       res.json(temporaryPlace)
     })
@@ -100,7 +46,7 @@ var TemporaryPlaceController = {
 
         tempPlace.save((err, tempPlace) => {
           if (err)
-            res.send(err)
+            res.render('error', { error: err })
 
           res.json(tempPlace)
         })
@@ -111,6 +57,5 @@ var TemporaryPlaceController = {
       })
   }
 }
->>>>>>> Stashed changes
 
-module.exports = TemporaryPlaceController;
+module.exports = TemporaryPlaceController
--- test/temporaryPlace.js
+++ test/temporaryPlace.js
@@ -1,51 +1,51 @@
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = 'test'
 
-var chai = require('chai');
-var chaiHttp = require('chai-http');
+var chai = require('chai')
+var chaiHttp = require('chai-http')
 
-var Promise = require("bluebird");
+var Promise = require('bluebird')
 
-var server = require('../bin/www');
-var User = require("../models/user");
-var Organization = require("../models/organization");
-var TemporaryPlace = require("../models/temporaryPlace");
+var server = require('../bin/www')
+var User = require('../models/user')
+var Organization = require('../models/organization')
+var TemporaryPlace = require('../models/temporaryPlace')
 
-var should = chai.should();
-chai.use(chaiHttp);
+var should = chai.should()
+chai.use(chaiHttp)
 
 var agent = chai.request.agent(server)
 
 describe('TemporaryPlace', () => {
-  
-  var user_id;
-  var user_email;
-  var org_id;
-  var tempplace_id;
+
+  var user_id
+  var user_email
+  var org_id
+  var tempplace_id
 
   beforeEach((done) => {
 
     Promise.resolve(['teste@teste.com.br', '12345']).spread((email, senha) => {
-      var newUser = new User();
+      var newUser = new User()
 
-      newUser.local.email = email;
-      newUser.local.password = newUser.generateHash(senha);
-      newUser.stage = 1;
+      newUser.local.email = email
+      newUser.local.password = newUser.generateHash(senha)
+      newUser.stage = 1
 
-      return newUser.saveAsync();
+      return newUser.saveAsync()
     }).then((user) => {
 
-      user_id = user._id;
-      user_email = user.local.email;
+      user_id = user._id
+      user_email = user.local.email
 
-      var newOrg = new Organization();
-      newOrg.name = 'Org01';
+      var newOrg = new Organization()
+      newOrg.name = 'Org01'
 
-      newOrg.saveAsync();
+      newOrg.saveAsync()
 
-      return ([user, newOrg]);
+      return ([user, newOrg])
     }).then((objs) => {
 
-      org_id = objs[1]._id;
+      org_id = objs[1]._id
 
       var address = {
         country: 'Brazil',
@@ -55,7 +55,7 @@ describe('TemporaryPlace', () => {
         street: 'Praça da Liberdade',
         number: '450',
         complement: ''
-      };
+      }
 
       var user = {
         _id: objs[0]._id,
@@ -64,98 +64,91 @@ describe('TemporaryPlace', () => {
         code: '',
         email: objs[0].local.email,
         phones: []
-      };
+      }
 
-      var temporaryPlace = new TemporaryPlace();
+      var temporaryPlace = new TemporaryPlace()
 
-      temporaryPlace.organizationId = org_id;
-      temporaryPlace.user = user;
-      temporaryPlace.description = 'Description';
-      temporaryPlace.capacity = 3;
-      temporaryPlace.address = address;
-      
-      return temporaryPlace.saveAsync();
+      temporaryPlace.organizationId = org_id
+      temporaryPlace.user = user
+      temporaryPlace.description = 'Description'
+      temporaryPlace.capacity = 3
+      temporaryPlace.address = address
+
+      return temporaryPlace.saveAsync()
     }).then((temporaryPlace) => {
-      tempplace_id = temporaryPlace._id;
-      done();
-    });
+      tempplace_id = temporaryPlace._id
+      done()
+    })
+  })
 
-  });
-  
   afterEach((done) => {
-    User.collection.drop();
-    Organization.collection.drop();
-    TemporaryPlace.collection.drop();
-    done();
-  });
-   
-  it('should list ALL TemporaryPlaces on /tempplaces/orgs/<id> GET', (done) => {    
+    User.collection.drop()
+    Organization.collection.drop()
+    TemporaryPlace.collection.drop()
+    done()
+  })
+
+  it('should list ALL TemporaryPlaces on /tempplaces/orgs/<id> GET', (done) => {
     agent
       .post('/login')
       .send({ email: 'teste@teste.com.br', password: '12345' })
       .then((res) => {
-        res.should.have.status(200);
+        res.should.have.status(200)
       })
       .then((res) => {
         agent
           .get('/tempplaces/orgs/' + org_id)
           .then((res) => {
-            res.should.have.status(200);
-            done();
-          })      
+            res.should.have.status(200)
+            done()
+          })
           .catch((err) => {
-            // console.log(err);
-            done(err);
-            // throw err;
-          });
+            // console.log(err)
+            done(err)
+          // throw err
+          })
       })
       .catch((err) => {
-        // console.log(err);
-        done(err);
-        // throw err;
-      });
-  });
+        // console.log(err)
+        done(err)
+      // throw err
+      })
+  })
 
-  it('should list a SINGLE TemporaryPlace on /tempplaces/<id> GET', (done) => {    
+  it('should list a SINGLE TemporaryPlace on /tempplaces/<id> GET', (done) => {
     agent
       .post('/login')
       .send({ email: 'teste@teste.com.br', password: '12345' })
       .then((res) => {
-        res.should.have.status(200);
+        res.should.have.status(200)
       })
       .then((res) => {
         agent
           .get('/tempplaces/' + tempplace_id)
           .then((res) => {
-<<<<<<< Updated upstream
-            res.should.have.status(200);
-            console.log(res.body);
-            done();
-=======
             res.should.have.status(200)
             // console.log(res)
             done()
->>>>>>> Stashed changes
           })
           .catch((err) => {
-            // console.log(err);
-            done(err);
-            // throw err;
-          });
+            // console.log(err)
+            done(err)
+          // throw err
+          })
       })
       .catch((err) => {
-        // console.log(err);
-        done(err);
-        // throw err;
-      });
-  });
+        // console.log(err)
+        done(err)
+      // throw err
+      })
+  })
 
-  it('should add a SINGLE TemporaryPlace on /tempplaces/<id> POST', function(done) {
+  it('should add a SINGLE TemporaryPlace on /tempplaces/<id> POST', function (done) {
     agent
       .post('/login')
       .send({ email: 'teste@teste.com.br', password: '12345' })
       .then((res) => {
-        res.should.have.status(200);
+        res.should.have.status(200)
       })
       .then((res) => {
         var address = {
@@ -166,7 +159,7 @@ describe('TemporaryPlace', () => {
           street: 'Praça da Liberdade',
           number: '450',
           complement: ''
-        };
+        }
 
         var user = {
           _id: user_id,
@@ -175,51 +168,45 @@ describe('TemporaryPlace', () => {
           code: '',
           email: user_email,
           phones: []
-        };
+        }
 
-        var temporaryPlace = new TemporaryPlace();
-        temporaryPlace.organizationId = org_id;
-        temporaryPlace.user = user;
-        temporaryPlace.description = 'Description';
-        temporaryPlace.capacity = 3;
-        temporaryPlace.address = address;
+        var temporaryPlace = new TemporaryPlace()
+        temporaryPlace.organizationId = org_id
+        temporaryPlace.user = user
+        temporaryPlace.description = 'Description'
+        temporaryPlace.capacity = 3
+        temporaryPlace.address = address
 
         agent
           .post('/tempplaces')
           .send(temporaryPlace)
           .then((res) => {
-<<<<<<< Updated upstream
-            res.should.have.status(200);
-            console.log(res.body);
-            done();
-=======
             // console.log(res.body)
             res.should.have.status(200)
             res.body.should.have.property('_id')
             res.body.should.have.property('organizationId')
             res.body.organizationId.should.be.equal(org_id.toString())
             done()
->>>>>>> Stashed changes
           })
           .catch((err) => {
-            // console.log(err);
-            done(err);
-            // throw err;
-          });
+            // console.log(err)
+            done(err)
+          // throw err
+          })
       })
       .catch((err) => {
-        // console.log(err);
-        done(err);
-        // throw err;
-      });
-  });
+        // console.log(err)
+        done(err)
+      // throw err
+      })
+  })
 
-  it('should update a SINGLE TemporaryPlace on /tempplaces/<id> PUT', function(done) {
+  it('should update a SINGLE TemporaryPlace on /tempplaces/<id> PUT', function (done) {
     agent
       .post('/login')
       .send({ email: 'teste@teste.com.br', password: '12345' })
       .then((res) => {
-        res.should.have.status(200);
+        res.should.have.status(200)
       })
       .then((res) => {
         var address = {
@@ -230,7 +217,7 @@ describe('TemporaryPlace', () => {
           street: 'Praça da Liberdade',
           number: '450',
           complement: ''
-        };
+        }
 
         var user = {
           _id: user_id,
@@ -239,43 +226,36 @@ describe('TemporaryPlace', () => {
           code: '',
           email: user_email,
           phones: []
-        };
+        }
 
-        var temporaryPlace = new TemporaryPlace();
-        temporaryPlace.organizationId = org_id;
-        temporaryPlace.user = user;
-        temporaryPlace.description = 'Description2';
-        temporaryPlace.capacity = 10;
-        temporaryPlace.address = address;
+        var temporaryPlace = new TemporaryPlace()
+        temporaryPlace.organizationId = org_id
+        temporaryPlace.user = user
+        temporaryPlace.description = 'Description2'
+        temporaryPlace.capacity = 10
+        temporaryPlace.address = address
 
         agent
           .put('/tempplaces/' + tempplace_id)
           .send(temporaryPlace)
           .then((res) => {
-<<<<<<< Updated upstream
-            res.should.have.status(200);
-            console.log(res.body);
-            done();
-=======
             res.should.have.status(200)
             res.body.should.have.property('_id')
             res.body.should.have.property('organizationId')
             res.body.organizationId.should.be.equal(org_id.toString())
             res.body.description.should.be.equal('Description2')
             done()
->>>>>>> Stashed changes
           })
           .catch((err) => {
-            // console.log(err);
-            done(err);
-            // throw err;
-          });
+            // console.log(err)
+            done(err)
+          // throw err
+          })
       })
       .catch((err) => {
-        // console.log(err);
-        done(err);
-        // throw err;
-      });
-  });
-
-});
+        // console.log(err)
+        done(err)
+      // throw err
+      })
+  })
+})
