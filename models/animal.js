fd94a2bb38844d99655107ff75fa6de7b78cfaf8
olavoshibata <olavoshibata@gmail.com>
2016-07-26 23:38:12 -0300

    test animal

--- models/animal.js
+++ models/animal.js
@@ -6,11 +6,15 @@ var animalSchema = new Schema({
   name: {type: String, trim: true, required: false},
   numberChip: {type: String, trim: true, required: false},
   category: {type: String, trim: true, required: false}, // (cachorro, gato, outros)
-  type: {type: String, trim: true, required: false}, // (raça)
+  breed: {type: String, trim: true, required: false}, // (raça)
+  type: {type: String, trim: true, required: false}, // (frajola, tricolor, ...)
   dateBorn: {type: Date, required: false},
   colors: [String], // (amarelo, branco, preto, um ou mais cores)
   fur: {type: String, trim: true, required: false}, // (curto, médio, longo)
   size: {type: String, trim: true, required: false}, // (pequeno, medio, grande)
+  weight: {type: Number, required: false},
+  height: {type: Number, required: false},
+  length: {type: Number, required: false},
   neutered: {type: Boolean, required: false}, // (sim, nao)
   vaccinated: {type: Boolean, required: false}, // (sim, nao)
   vermifugated: {type: Boolean, required: false}, // (sim, nao)
--- test/animal.js
+++ test/animal.js
@@ -8,14 +8,14 @@ var Promise = require('bluebird')
 var server = require('../bin/www')
 var User = require('../models/user')
 var Organization = require('../models/organization')
-var Accountability = require('../models/accountability')
+var Animal = require('../models/animal')
 
 var should = chai.should()
 chai.use(chaiHttp)
 
 var agent = chai.request.agent(server)
 
-describe('Accountability', () => {
+describe('Animal', () => {
 
   var user_id
   var org_id
@@ -26,6 +26,7 @@ describe('Accountability', () => {
     newOrg.name = 'Org01'
 
     newOrg.save((err) => {
+
       org_id = newOrg._id
 
       var newUser = new User()
@@ -45,11 +46,11 @@ describe('Accountability', () => {
   after((done) => {
     User.collection.drop()
     Organization.collection.drop()
-    Accountability.collection.drop()
+    Animal.collection.drop()
     done()
   })
 
-  it('should add a SINGLE accountability on /account POST', function (done) {
+  it('should add a SINGLE animal on /animal POST', function (done) {
     agent
       .post('/login')
       .send({ email: 'teste@teste.com.br', password: '12345' })
@@ -57,22 +58,38 @@ describe('Accountability', () => {
         res.should.have.status(200)
       })
       .then((res) => {
-        var accountability = new Accountability()
-        accountability.organizationId = org_id
-        accountability.account = 12345
-        accountability.description = 'Description'
-        // accountability.entryDate = 
-        accountability.entryValue = 1.00
-        accountability.userId = user_id
-        // accountability.files = 
+        var animal = new Animal()
+        animal.organizationId = org_id
+        animal.name = 'Neko'
+        animal.numberChip = '12345'
+        animal.category = 'cat'
+        animal.breed = 'srd'
+        animal.type = 'tricolor'
+        animal.dateBorn = Date()
+        animal.colors = ['amarelo', 'preto', 'branco']
+        animal.fur = 'médio'
+        animal.size = 'médio'
+        animal.weight = 5
+        animal.height = 3.5
+        animal.length = 25.5
+        animal.neutered = true
+        animal.vaccinated = true
+        animal.vermifugated = true
+        animal.history = 'She was finded in a street'
+        // animal.photos
+        // animal.specitalAdoption
+        animal.gender = 'f'
+        animal.sociable = 3
+        animal.playful = 4
+        animal.affectionate = 5
+        // animal.temporaryPlace    
+        // animal.shelter._id = 
 
         agent
-          .post('/account')
-          .send(accountability)
+          .post('/animal')
+          .send(animal)
           .then((res) => {
             res.should.have.status(200)
-            account_id = res.body._id
-            // console.log(res.body)
             done()
           })
           .catch((err) => {
@@ -87,7 +104,7 @@ describe('Accountability', () => {
       // throw err
       })
   })
-
+/*
   it('should list ALL accountability on /account?orgId=<id> GET', (done) => {
     agent
       .post('/login')
@@ -186,4 +203,5 @@ describe('Accountability', () => {
       // throw err
       })
   })
+*/
 })
